82da01bcd1added3226bc93b61ac6bd4a70168d7
elias-yousef <elias2005new@gmail.com>
2026-08-22 08:27:50 -0400

    .

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -4,3 +4,19 @@ set -e
 
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+#temp dir that stores PID and socker files while runing
+mkdir -p /run/mysqild
+
+#change the ownership from root to the user and group (mysql)
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysqld
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing Mariadb data directory...."
+    
+
+fi
+
+
+
